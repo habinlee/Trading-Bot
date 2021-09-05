ad2f81e2cf5a4951bd7ed9e758eb7b48bd6dd525
Kevin <88265967+habinlee@users.noreply.github.com>
2021-09-05 14:55:15 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,99 @@
+from logging_system import LoggerFile
+from pretty_printer import PPrint
+from upbitworker import Upbit
+from ai_model import Predictor
+import time
+import datetime
+import sys
+import schedule
+
+if __name__ == '__main__':
+
+    # Printer
+    pprinter = PPrint()
+
+    # Logging process
+    logger_file = LoggerFile()
+    logger = logger_file.logging()
+
+    # logger_file.prevent_reload()
+
+    # Upbit Main function
+    upbit = Upbit()
+
+    now = datetime.datetime.now()
+    mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
+
+    print("Start the Upbit trading bot!")
+    logger.info("Start the Upbit trading bot!")
+
+    # AI model application
+    predictor = Predictor()
+
+    predictor.predict_close_price("KRW-DOGE")
+    schedule.every().hour.do(lambda: predictor.predict_close_price("KRW-DOGE"))  
+
+    while True:
+        try:
+            now = datetime.datetime.now()
+
+            k = upbit.get_k()
+
+            ma5 = upbit.get_ma5()
+
+            target_price = upbit.get_target(k)
+
+            schedule.run_pending()
+
+            if mid < now < mid + datetime.delta(seconds=10):
+
+                current_price = upbit.get_ticker({"markets" : "KRW-DOGE"})[0]['trade_price']
+
+                print("Curent price of the coin is {}".format(current_price))
+                logger.info("Curent price of the coin is {}".format(current_price))
+
+
+                mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
+
+                query = {
+                    'market': 'KRW-DOGE',
+                    'side': 'ask',
+                    'volume': str(upbit.get_asset_volume('KRW-DOGE')),
+                    'ord_type': 'market',
+                } 
+
+                sell_result = upbit.trade_coin(query)
+                print('The coin was sold!')
+                pprinter.pPrint(sell_result)
+                assets = upbit.request_private()
+                pprinter.pPrint_multi(assets)
+
+
+
+            balance = float(upbit.get_balance())
+
+            print('Your current balance in KRW is : {}'.format(balance))
+            logger.info('Your current balance in KRW is : {}'.format(balance))
+
+            if (current_price > target_price) and (current_price > ma5) and (balance * 0.9995 > current_price) and (current_price < predictor.predicted_price):
+                
+                query = {
+                    'market': 'KRW-DOGE',
+                    'side': 'bid',
+                    'volume': str((balance * 0.9995) / current_price),
+                    'price': str(current_price),
+                    'ord_type': 'limit',
+                }
+
+                buy_result = upbit.trade_coin(query)
+                print('The coin was bought!')
+                pprinter.pPrint(sell_result)
+                assets = upbit.request_private()
+                pprinter.pPrint_multi(assets)
+
+        except:
+            print('Error - Terminating Process!')
+            logger.info('Error - Terminating Process!')
+            sys.exit(1)
+
+        time.sleep(1)
